7740af3225aecabc1d15c1b39cd56a8c2f699b7a
YehyunLee <glad7cu@gmail.com>
2023-09-11 15:42:23 -0400

    refactored to extract helper method

---
+++ src/WhileFizzBuzz.java
@@ -0,0 +1,40 @@
+/**
+ * Solve the FizzBuzz challenge.
+ */
+class WhileFizzBuzz {
+
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 99) {
+            count = doFizzBuzz(count);
+        }
+    }
+
+    private static int doFizzBuzz(int count) {
+        count += 1;
+
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = count % 3 == 0;
+        boolean divisibleBy5 = count % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(count);
+
+        }
+        return count;
+    }
+}
